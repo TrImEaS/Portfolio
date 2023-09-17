4a93706f58d3f0b78c34245dd6d0f8968e406dd4
TrImEaS <trimeas@hotmail.com>
2023-09-17 04:02:33 -0300

    Ending intro page
    Ok

--- app.js
+++ app.js
@@ -2,13 +2,43 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let i = 0;
+
     function typeWriter() {
-        let txt = 'Web Developer';
-        let speed = 60;
+        let txt = 'Frontend Developer';
+        let speed = 100;
+
         if (i < txt.length) {
             document.getElementById("typewriter").textContent += txt.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         }
-    } setTimeout(typeWriter, 900)
+    } setTimeout(typeWriter, 50);
 })
+
+//Hover for logo
+
+function setLogoStyles(color, borderColor) {
+    const logo = document.getElementById('nav_logo_t');
+    logo.style.color = color;
+    logo.style.borderColor = borderColor;
+}
+
+let hero = document.getElementById('nav_hero');
+
+hero.addEventListener('mouseover', () => {
+    setLogoStyles("#5cd9e2", "#5cd9e2");
+});
+
+hero.addEventListener('mouseleave', () => {
+    setLogoStyles("#fff", "#fff");
+});
+
+
+//Changes nav when user isn't in top
+
+window.addEventListener('scroll', () =>{
+    let nav = document.getElementById('nav');
+    if (window.scrollY > 100) {nav.style.backgroundColor = '#080c18';} 
+    else {nav.style.backgroundColor = 'transparent';}
+});
+
