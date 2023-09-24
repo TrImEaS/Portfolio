0f79bb4d8a186c8319ab698869647f7bce86a430
TrImEaS <trimeas@hotmail.com>
2023-09-24 01:35:39 -0300

    Standard, eslint, and some vscode configs added

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-//Effect TypeWriter function for welcome txt every page load 
+// Effect TypeWriter function for welcome txt every page load
 
 document.addEventListener('DOMContentLoaded', () => {
-    let i = 0;
+  let i = 0
 
-    function typeWriter() {
-        let txt = 'Frontend Developer';
-        let speed = 60;
+  function typeWriter () {
+    const txt = 'Frontend Developer'
+    const speed = 60
 
-        if (i < txt.length) {
-            document.getElementById("typewriter").textContent += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    } setTimeout(typeWriter, 0);
+    if (i < txt.length) {
+      document.getElementById('typewriter').textContent += txt.charAt(i)
+      i++
+      setTimeout(typeWriter, speed)
+    }
+  }
+  setTimeout(typeWriter, 0)
 })
 
-//Hover for logo
+// Hover for logo
 
-function setLogoStyles(color, borderColor) {
-    const logo = document.getElementById('nav_logo_t');
-    logo.style.color = color;
-    logo.style.borderColor = borderColor;
+function setLogoStyles (color, borderColor) {
+  const logo = document.getElementById('nav_logo_t')
+  logo.style.color = color
+  logo.style.borderColor = borderColor
 }
 
-let hero = document.getElementById('nav_hero');
+const hero = document.getElementById('nav_hero')
 
 hero.addEventListener('mouseover', () => {
-    setLogoStyles("#5cd9e2", "#5cd9e2");
-});
+  setLogoStyles('#5cd9e2', '#5cd9e2')
+})
 
 hero.addEventListener('mouseleave', () => {
-    setLogoStyles("#fff", "#fff");
-});
+  setLogoStyles('#fff', '#fff')
+})
 
+// Changes nav when user isn't in top
 
-//Changes nav when user isn't in top
-
-window.addEventListener('scroll', () =>{
-    let nav = document.getElementById('nav');
-    if (window.scrollY > 100) {nav.style.backgroundColor = '#080c18';} 
-    else {nav.style.backgroundColor = 'transparent';}
-});
-
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('nav')
+  if (window.scrollY > 100) {
+    nav.style.backgroundColor = '#080c18'
+  } else {
+    nav.style.backgroundColor = 'transparent'
+  }
+})
