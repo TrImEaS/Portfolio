ff9b0bc9a010066e6272154b5e1752569b724eec
TrImEaS <trimeas@hotmail.com>
2024-01-21 01:52:05 -0300

    fix attemp 3

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs/promises';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist',
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
